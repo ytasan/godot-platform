48cd7e255a195e7f8a623ca6cc4a5f7cc04ed178
ytasan <info@yasintasan.com>
2024-07-21 17:47:05 +0300

    player animation direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,9 +18,14 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
